4dcd01f352733581696230fa59d943c1d6bc8b69
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-06-07 18:57:34 -0300

    Exercise solution

--- src/main/java/SumOfMultiples.java
+++ src/main/java/SumOfMultiples.java
@@ -1,11 +1,27 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class SumOfMultiples {
 
+    private final int number;
+    private final Set<Integer> set;
+
     SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.number = number;
+        this.set = Arrays.stream(set).filter(i -> i != 0).boxed().collect(Collectors.toSet());
     }
 
     int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Function<Integer, IntStream> multiples = n -> IntStream.iterate(n, i -> n + i)
+                .limit((number - 1) / n);
+
+        return set.stream()
+                .flatMapToInt(multiples)
+                .distinct()
+                .sum();
     }
 
 }
